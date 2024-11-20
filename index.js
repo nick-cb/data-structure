eacde650b2af0d526dbf4bfc379d0a14cef06162
VI Nguyen <nguyenhungvicb@gmail.com>
2024-11-20 08:10:45 +0700

    update

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-const process = require("process");
-const util = require("util");
-// B-Tree
-/* B-Tree order `m`:
- * - The root is either leaf or has at least 2 children
- * - Each internal node, except for the root, has between [m/2] and m children
-* - All leaf are at the same level
- * Search a node
- * - Start from the root node
- * - Go though each element of the node and compare to the needed search value
- * - if (val < elements[0]) goToLeftMostPointer()
- * - if (val > elements[-1]) goToRightMostPointer()
- * - if (val >= elements[i] && val < elements[i+1]) goToPointer(elements[i])
-
- * Insert a node
- * - Start from the root node
- * - Find the position in which the node should be insert to the parent node
- * - If there is a child node at that position, try insert this new node to that child node
- * - If there is no child node at that position, insert new node to the that position
- * - If the length of the parent node > max children, split the children into 3 node
- * - Make the 2 new node childrent of the middle node
- * - If parent node is None -> make that middle node to be root node
- * - Else insert that middle node into parent node
- */
-
 class Node {
   /** @type {Array<InternalNode>} internalNodes */
   internalNodes;
@@ -124,13 +99,10 @@ class BTree {
     if (!result || result.length === 0) {
       return;
     }
-    // console.log(util.inspect({ result }, { colors: true, depth: 12 }));
 
     result[0].children[result[0].internalNodes[0].key] = result[1];
     result[0].children[key] = result[2];
 
-    // result.children[key] = result.children['last']
-    // delete result.children['last'];
     delete parentNode.children[key];
     return this.#insert(parentNode, result[0]);
   }
@@ -175,32 +147,3 @@ class BTree {
 }
 
 module.exports = BTree;
-
-// const btree = new BTree();
-// console.log(btree);
-// btree.insert(2);
-// btree.insert(4);
-// btree.insert(7);
-// btree.insert(9);
-// // console.log(btree);
-// btree.insert(1);
-// // console.log(util.inspect(btree, { colors: true, depth: 12 }));
-// btree.insert(5);
-// // console.log(util.inspect(btree, { colors: true, depth: 12 }));
-// btree.insert(0);
-// // console.log(util.inspect(btree, { colors: true, depth: 12 }));
-// btree.insert(3);
-// // console.log(util.inspect(btree, { colors: true, depth: 12 }));
-// btree.insert(-1);
-// btree.insert(10);
-// btree.insert(11);
-// btree.insert(12);
-// btree.insert(13);
-// btree.insert(14);
-// btree.insert(15);
-// btree.insert(16);
-// btree.insert(17);
-// btree.insert(18);
-// btree.insert(19);
-// btree.insert(20);
-// console.log(util.inspect(btree, { colors: true, depth: 12 }));
